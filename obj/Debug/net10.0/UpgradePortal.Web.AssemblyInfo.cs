7a7a97dcf839db2077506b2883ddcb27194b81d0
Rauf Ibrahimkhail <rauf.ibrahimkhail@intrahealth.com>
2026-05-01 17:48:33 -0700

    Your commit message here

--- obj/Debug/net10.0/UpgradePortal.Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/UpgradePortal.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UpgradePortal.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e693d20e219030bfeeaaf1d63f6a4c63c6168e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ccae792f1f9952e9b8ff94f698c3b160a04afb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UpgradePortal.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UpgradePortal.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
